9dad6b38c099d5dacba2e83d9aa6327303ed2867
diego Hernandez <diego.hernandez.e@usach.cl>
2025-05-13 23:27:18 -0400

    Se inician proyectos por microservicios vacios pendientes de configuracion

---
+++ microservicios/tarifas/src/main/java/com/diegomuu/karting/tarifas/TarifasApplication.java
@@ -0,0 +1,13 @@
+package com.diegomuu.karting.tarifas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarifasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarifasApplication.class, args);
+	}
+
+}
---
+++ microservicios/tarifasFestivos/src/main/java/com/diegomuu/karting/tarifasFestivos/TarifasFestivosApplication.java
@@ -0,0 +1,13 @@
+package com.diegomuu.karting.tarifasFestivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarifasFestivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarifasFestivosApplication.class, args);
+	}
+
+}
