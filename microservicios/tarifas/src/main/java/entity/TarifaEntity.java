94c972b2aecd516e66bddcba1c4e949f48768519
Diego <diego.hernandez.e@usach.cl>
2025-05-16 17:50:45 -0400

    hot fix 3

--- microservicios/tarifas/src/main/java/entity/TarifaEntity.java
+++ microservicios/tarifas/src/main/java/entity/TarifaEntity.java
@@ -1,10 +1,14 @@
 package entity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tarifas")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TarifaEntity {
 
     @Id
