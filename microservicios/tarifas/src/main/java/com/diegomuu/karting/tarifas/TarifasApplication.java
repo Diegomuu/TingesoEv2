c01e42b22093d744f50a12dfc50f8ea28bbc889e
Diego <diego.hernandez.e@usach.cl>
2025-05-16 21:02:26 -0400

    hot fix

--- microservicios/tarifas/src/main/java/com/diegomuu/karting/tarifas/TarifasApplication.java
+++ microservicios/tarifas/src/main/java/com/diegomuu/karting/tarifas/TarifasApplication.java
@@ -3,12 +3,11 @@ package com.diegomuu.karting.tarifas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.diegomuu.karting.tarifas.entity")
-@EnableJpaRepositories
-
+@EnableDiscoveryClient
 public class TarifasApplication {
 
 	public static void main(String[] args) {
