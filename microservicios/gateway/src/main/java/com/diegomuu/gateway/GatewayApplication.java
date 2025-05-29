1ce76ed0c66e1a91aa240944e5377d642d9ee197
Diego <diego.hernandez.e@usach.cl>
2025-05-29 19:41:11 -0400

    hot fix

--- microservicios/gateway/src/main/java/com/diegomuu/gateway/GatewayApplication.java
+++ microservicios/gateway/src/main/java/com/diegomuu/gateway/GatewayApplication.java
@@ -2,6 +2,7 @@ package com.diegomuu.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,18 +10,9 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class GatewayApplication {
 
-
-	@Configuration
-	public class RestTemplateConfig {
-		@Bean
-		public RestTemplate restTemplate() {
-			return new RestTemplate();
-		}
-	}
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
+
