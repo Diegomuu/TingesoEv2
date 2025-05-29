2e4bc13c22cae3af5fe32fb26d6c86e5031356b5
Diego <diego.hernandez.e@usach.cl>
2025-05-29 19:18:01 -0400

    hot fix

---
+++ microservicios/gateway/src/main/java/com/diegomuu/gateway/DTO/TarifaDTO.java
@@ -0,0 +1,10 @@
+package com.diegomuu.gateway.DTO;
+
+
+public class TarifaDTO {
+    private Long id;
+    private int vueltas;
+    private int tiempoMaxMinutos;
+    private double precio;
+}
+
--- microservicios/gateway/src/main/java/com/diegomuu/gateway/GatewayApplication.java
+++ microservicios/gateway/src/main/java/com/diegomuu/gateway/GatewayApplication.java
@@ -3,13 +3,20 @@ package com.diegomuu.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 
-
+	@Configuration
+	public class RestTemplateConfig {
+		@Bean
+		public RestTemplate restTemplate() {
+			return new RestTemplate();
+		}
+	}
 
 
 	public static void main(String[] args) {
